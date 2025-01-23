a5010b1ac8848c6cfcca6496ee3aa21227bf5464
morski13 <radiusmill@gmail.com>
2025-01-23 13:22:00 +0100

    Iymene u Handler.php-u, kako bi se greske vracale u JSON formatu

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,29 +4,22 @@ namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Throwable;
+use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class Handler extends ExceptionHandler
 {
     /**
-     * A list of exception types with their corresponding custom log levels.
-     *
-     * @var array<class-string<\Throwable>, \Psr\Log\LogLevel::*>
-     */
-    protected $levels = [
-        //
-    ];
-
-    /**
      * A list of the exception types that are not reported.
      *
-     * @var array<int, class-string<\Throwable>>
+     * @var array<int, class-string<Throwable>>
      */
     protected $dontReport = [
         //
     ];
 
     /**
-     * A list of the inputs that are never flashed to the session on validation exceptions.
+     * A list of the inputs that are never flashed for validation exceptions.
      *
      * @var array<int, string>
      */
@@ -46,5 +39,39 @@ class Handler extends ExceptionHandler
         $this->reportable(function (Throwable $e) {
             //
         });
+    }
+
+    public function render($request, Throwable $exception)
+    {
+        if ($request->wantsJson()) {
+            $statusCode = 500;
+            $message = 'Server Error'; // Default poruka
+
+            if ($exception instanceof HttpException) {
+                $statusCode = $exception->getStatusCode();
+                $message = $exception->getMessage();
+            } elseif ($exception instanceof \Illuminate\Validation\ValidationException) { // Dodato za validacione izuzetke
+                return response()->json([
+                    'errors' => $exception->errors(),
+                    'message' => 'The given data was invalid.',
+                ], 422);
+            } elseif ($exception instanceof \Illuminate\Auth\AuthenticationException){
+                return response()->json([
+                    'message' => 'Unauthenticated',
+                ], 401);
+            }
+            else if (config('app.debug')) {
+                $message = $exception->getMessage(); // prikazuje detaljnu poruku samo u debug modu
+            }
+
+            return response()->json([
+                'error' => [
+                    'message' => $message,
+                    'status_code' => $statusCode,
+                ]
+            ], $statusCode);
+        }
+
+        return parent::render($request, $exception);
     }
 }
