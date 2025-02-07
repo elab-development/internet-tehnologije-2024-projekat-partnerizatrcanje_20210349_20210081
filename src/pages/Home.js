11cda32f9f4d0b03f5e5f3dd9cca674fb6723eb5
Savin-2 <nemanjasavin2@gmail.com>
2025-02-07 13:40:59 +0100

    home pokusaj

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,12 @@
-// src/pages/Home.js
 import React from 'react';
 import CommentSection from '../components/CommentSection';
 import Feed from '../components/Feed';
-import NavBar from '../components/NavBar';
 import Post from '../components/Post';
 import RunningPlanCard from '../components/RunningPlanCard';
 
 const Home = () => {
   return (
-    <div>
-      <NavBar />
+    <div className="home-container">
       <h1>Dobrodošli na Home stranicu</h1>
       <Feed />
       <RunningPlanCard />
