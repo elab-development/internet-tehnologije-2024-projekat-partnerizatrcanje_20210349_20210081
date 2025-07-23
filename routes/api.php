245cd60f39b27a2d288684e13bfc6cf731d84f18
morski13 <radiusmill@gmail.com>
2025-07-23 15:13:32 +0200

    CORS

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,7 @@
 <?php
+header('Access-Control-Allow-Origin: http://localhost:3000');
+header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+header('Access-Control-Allow-Headers: Content-Type, Authorization');
 
 use App\Models\User;
 use Illuminate\Support\Facades\Auth;
