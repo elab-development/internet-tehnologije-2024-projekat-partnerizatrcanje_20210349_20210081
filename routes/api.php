31f688a88ab1ac3873c140cf7bf3b7ec5154ff74
morski13 <radiusmill@gmail.com>
2025-07-23 15:17:25 +0200

    CORS Handle 2

--- routes/api.php
+++ routes/api.php
@@ -1,4 +1,12 @@
 <?php
+// Handle CORS
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    header('Access-Control-Allow-Origin: http://localhost:3000');
+    header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
+    header('Access-Control-Allow-Headers: Content-Type, Authorization');
+    exit(0);
+}
+
 header('Access-Control-Allow-Origin: http://localhost:3000');
 header('Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS');
 header('Access-Control-Allow-Headers: Content-Type, Authorization');
